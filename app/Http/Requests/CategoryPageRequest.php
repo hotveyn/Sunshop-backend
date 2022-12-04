ec4aa03d1ea3211ba99aa66e05ab04fcf577a4d6
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-04 17:51:15 +0300

    feat: Request для Category

---
+++ app/Http/Requests/CategoryPageRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryPageRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "skip" => ["required", "integer"],
+            "take" => ["required", "integer"],
+        ];
+    }
+}
