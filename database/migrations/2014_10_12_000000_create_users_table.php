e713fb67581bc1682bdfffa1d6d171d3e0b06f5a
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-04 17:41:52 +0300

    fix: Изменение именований полей

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum("role", ["staff", "user"])->default("user");
-            $table->float("balance")->default(0);
             $table->string("login")->unique();
             $table->string("nickname");
+            $table->enum("type", ["staff", "user"])->default("user");
+            $table->float("balance")->default(0);
             $table->string("password");
-            $table->rememberToken();
+            $table->string("api_token")->nullable();
             $table->timestamps();
         });
     }
