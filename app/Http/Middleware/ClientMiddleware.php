fe6336c17c0cb92c314e0bcf81e5b2baa15bc774
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-04 17:48:00 +0300

    feat: Middleware для User типа "user"

---
+++ app/Http/Middleware/ClientMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
+class ClientMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        throw_if(
+            !auth()->check() || $request->user()->type !== "user",
+            new AuthenticationException("Only client allowed to perform this action")
+        );
+
+        return $next($request);
+    }
+}
