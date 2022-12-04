f1a1afe18a29d29f1466d1068ee301ff8714ac45
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-04 17:54:17 +0300

    feat: Инициализация всех готов маршрутов API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post("register", [\App\Http\Controllers\User\UserController::class, "store"]);
+Route::post("login", [\App\Http\Controllers\User\UserController::class, "login"]);
+
+
+
+Route::prefix("/category")->group(function (){
+    Route::get("/", [\App\Http\Controllers\CategoryController::class,"info"]);
+    Route::post("/", [\App\Http\Controllers\CategoryController::class,"infoPaged"]);
+});
+
+Route::middleware('auth:api')->group(function (){
+    Route::get("session", [\App\Http\Controllers\User\UserController::class, "session"]);
+    Route::get("logout", [\App\Http\Controllers\User\UserController::class, "logout"]);
+
+    Route::middleware("staff")->group(function (){
+        Route::prefix("/user")->group(function (){
+            Route::put("/{id}", [\App\Http\Controllers\User\StaffType\UserController::class, "update"]);
+            Route::get("/{id}", [\App\Http\Controllers\User\StaffType\UserController::class, "infoOne"]);
+            Route::delete("/{id}", [\App\Http\Controllers\User\StaffType\UserController::class, "delete"]);
+            Route::post("/", [\App\Http\Controllers\User\StaffType\UserController::class, "infoPaged"]);
+            Route::get("/", [\App\Http\Controllers\User\StaffType\UserController::class, "info"]);
+        });
+
+        Route::prefix("/category")->group(function (){
+            Route::get("/", [\App\Http\Controllers\CategoryController::class,"info"]);
+            Route::post("/", [\App\Http\Controllers\CategoryController::class,"infoPaged"]);
+            Route::post("create", [\App\Http\Controllers\CategoryController::class,"create"]);
+            Route::delete("/{id}", [\App\Http\Controllers\CategoryController::class,"delete"]);
+            Route::put("/{id}", [\App\Http\Controllers\CategoryController::class,"update"]);
+        });
+        Route::prefix("/product")->group(function (){
+//           Route::post("/create", [\App\Http\Controllers\ProductController::class, "create"]);
+        });
+    });
+//    Route::middleware("client")->group(function (){
+//
+//    });
 });
