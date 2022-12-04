e131e6b2fde2937408710c48be785f0f3dff23d7
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-12-04 17:51:26 +0300

    feat: Resource для Category

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "hot" => $this->hot,
+            "children" => $this->when(!is_null($this->categoryChildren), CategoryResource::collection($this->categoryChildren)),
+            "parent_id" => $this->parent_id
+
+        ];
+    }
+}
